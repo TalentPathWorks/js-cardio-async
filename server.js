b3b465d3f14ef73290cb24cfb97fc48f6f42426b
Roberto Sanchez <rsanchez92@live.com>
2019-07-17 21:55:57 -0700

    Added Server Functionalities

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require(`http`);
-
+const url = require('url');
+const db = require('./db.js')
 const server = http.createServer();
 
 server.on('request',(request,response)=>{
@@ -13,7 +14,48 @@ server.on('request',(request,response)=>{
       'Content-type': 'application/json',
       'Another-Header': 'more Things',
     })
-    response.end(JSON.stringify(status));
+    return response.end(JSON.stringify(status));
+  }
+  const parsedUrl = url.parse(request.url, true);
+  if(parsedUrl.pathname === '/get' && request.method === 'POST'){
+    return db.get(parsedUrl.query.file,parsedUrl.query.key)
+    .then(
+      response.end("ALL COMPLETED WELL")
+    )
+    .catch(
+      response.end("ERROR")
+    )
+    
+  }
+  if(parsedUrl.pathname === '/set' && request.method === 'PATCH'){
+    return db.set(parsedUrl.query.file,parsedUrl.query.key,parsedUrl.query.value)
+    .then(
+      response.end("ALL COMPLETED WELL")
+    )
+    .catch(
+      response.end("ERROR")
+    )
+  }
+  if(parsedUrl.pathname === '/remove' && request.method === 'DELETE'){
+    return db.remove(parsedUrl.query.file,parsedUrl.query.key)
+    .then(
+      response.end("ALL COMPLETED WELL")
+    )
+    .catch(
+      response.end("ERROR")
+    )
+  }
+  if(parsedUrl.pathname === '/deletefile' && request.method === 'DELETE'){
+    return db.deleteFile(parsedUrl.query.file,parsedUrl.query.key)
+    .then(
+      response.end("ALL COMPLETED WELL")
+    )
+    .catch(
+      response.end("ERROR")
+    )
+  }
+  if(parsedUrl.pathname === '/createfile' && request.method === 'POST'){
+    response.end("CREATE FILE")
   }
   
 })
